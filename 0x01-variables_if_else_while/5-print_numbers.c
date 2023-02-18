690344356103c063169a50010057925e05e9b41f
Emmanuel Ogbaa <ogbaae@gmail.com>
2023-02-18 01:43:01 -0800

    Prints all the digits of base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digits of base 10
+ * Return: 0 success
+ */
+
+int main(void)
+{
+	int x = 0;
+
+	while (x <= 9)
+	{
+		printf("%d", x);
+		x++;
+	}
+	return (0);
+}
