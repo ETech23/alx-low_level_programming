0eabdacaf37abfee1359307d40d407a42809859d
Emmanuel Ogbaa <ogbaae@gmail.com>
2023-02-26 12:53:04 -0800

    Multiplies two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: first input
+ * @b: second input
+ * Return: 0 or 1
+ */
+int mul(int a, int b)
+{
+		return (a * b);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -7,6 +7,7 @@ void print_alphabet_x10(void);
 int _isdigit(int c);
 int _islower(int c);
 int _isupper(int c);
+int mul(int a, int b);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
