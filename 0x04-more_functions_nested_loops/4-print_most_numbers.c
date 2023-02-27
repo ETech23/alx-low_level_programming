e3d66ad1c1a472b6c8b0f682f61f0d98fb5d4d5b
Emmanuel Ogbaa <ogbaae@gmail.com>
2023-02-26 21:23:53 -0800

    Prints some numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,8 +6,16 @@
  */
 void print_most_numbers(void)
 {
-	char c;
-	for (c >= '0' && c <='9' && (c != '2'; c != '4') c++)
-		putchar(c);
+	int c;
+
+	for (c = '0'; c <= '9')
+	{
+		if (c == '2' || c == '4')
+		{
+			continue;
+		}
+		else
+			putchar(c);
+	}
 	putchar('\n');
 }
