c0c96e20cd0854069dc9995d2d1b2fb0ef103cd3
Emmanuel Ogbaa <ogbaae@gmail.com>
2023-02-27 13:18:02 -0800

    Prints string followed by mee line

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - the function
+ * @str: the string
+ * Return: 0
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
