73d1dd2806e31d43ce9f22285c63d369b6f89791
Emmanuel Ogbaa <ogbaae@gmail.com>
2023-02-27 21:06:05 -0800

    Prints triangle

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -17,6 +17,7 @@ void print_numbers(void);
 void print_most_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
+void print_triangle(int size);
 void print_square(int size);
 void more_numbers(void);
 void times_table(void);
