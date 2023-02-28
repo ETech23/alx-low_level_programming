97c6fbd21168667638a0b9914b8288f913714eb1
Emmanuel Ogbaa <ogbaae@gmail.com>
2023-02-27 20:33:18 -0800

    Print more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,28 +2,25 @@
 #include <stdlib.h>
 
 /**
- *  * more_numbers - a function print a number.
- *   *
- *    */
-
+ * more_numbers - a function print a number.
+ */
 void more_numbers(void)
 {
-		int i, k;
+	int i, k;
 
-			for (k = 1; k <= 10; k++)
-					{
+	for (k = 1; k <= 10; k++)
+	{
 								/* Execution du code print_most_number */
-
-								for (i = 0; i <= 14; i++)
-											{
-															if (i <= 9)
-																				_putchar(i + '0');
-																		else
-																						{
-																											_putchar(i / 10 + '0');
-																															_putchar(i % 10 + '0');
-																																		}
-																				}
-										system("echo \"\"");
-											}
+		for (i = 0; i <= 14; i++)
+		{
+			if (i <= 9)
+				_putchar(i + '0');
+			else
+			{
+				_putchar(i / 10 + '0');
+				_putchar(i % 10 + '0');
+			}
+		}
+		system("echo \"\"");
+					}
 }
