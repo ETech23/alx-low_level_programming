9b8d04f9a21bca7dbc5263e4388aa680a5e16f2c
Emmanuel Ogbaa <ogbaae@gmail.com>
2023-02-27 20:43:44 -0800

    Prints line diagonal

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -16,6 +16,7 @@ void jack_bauer(void);
 void print_numbers(void);
 void print_most_numbers(void);
 void print_line(int n);
+void print_diagonal(int n);
 void more_numbers(void);
 void times_table(void);
 int add(int a, int b);
