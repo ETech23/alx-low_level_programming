0525e7491ce498748be35fcf36977b22368e8fba
Emmanuel Ogbaa <ogbaae@gmail.com>
2023-03-08 12:47:25 -0800

    Prints the length of string

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -4,13 +4,12 @@
  * *
  * * @s: the chaine
  * * Return: Always 0.
- * */
+ */
 
 int _strlen_recursion(char *s)
 {
-
-		if (*s == '\0')
-					return (0);
-			else
-						return (1 + _strlen_recursion(s + 1));
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
 }
