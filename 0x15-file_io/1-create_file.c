e09de2f5fd4cf2fe33d6e14fb44c9291b3fdc508
Emmanuel Ogbaa <ogbaae@gmail.com>
2023-04-12 09:04:10 -0700

    Creates file and add permission

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+/**
+ *  * create_file - a function ...
+ *   * @filename: the list
+ *    * @text_content: the number
+ *     *
+ *      * Return: 1 or 0
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
+	int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+
+	if (fd == -1)
+	{
+		return (-1);
+	}
+
+	if (text_content != NULL)
+	{
+		ssize_t bytes_written = write(fd, text_content, strlen(text_content));
+
+		if (bytes_written == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+
+	close(fd);
+	return (1);
+}
